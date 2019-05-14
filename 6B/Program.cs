b20e48ac6b14fcf4dd86b98b1926ab3f647a05d1
AkhileshNS <nsakhilesh02@gmail.com>
2019-05-14 17:53:25 +0530

    Added 6B

--- 6B/Program.cs
+++ 6B/Program.cs
@@ -2,11 +2,34 @@
 
 namespace _6B
 {
+    class Name {
+        public string firstName;
+        public string lastName;
+    }
+
     class Program
     {
+        delegate bool compare(Name n1, Name n2);
+
+        public static bool nameComparer(Name n1, Name n2) {
+            return n1.firstName.Equals(n2.firstName) && n1.lastName.Equals(n2.lastName); 
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Name[] names = new Name[5] {
+                new Name() {firstName = "Akhilesh", lastName = "NS"},
+                new Name() {firstName = "Akhilesh", lastName = "NS"},
+                new Name() {firstName = "Aniruddha", lastName = "MN"},
+                new Name() {firstName = "Anirban", lastName = "G"},
+                new Name() {firstName = "Anirban", lastName = "D"}, 
+            };
+
+            compare Compare = new compare(nameComparer);
+            Console.WriteLine(Compare(names[0], names[1]));
+            Console.WriteLine(Compare(names[2], names[3]));
+            Console.WriteLine(Compare(names[3], names[4]));
+
         }
     }
 }
